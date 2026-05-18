deba1d2bdd1da94da044900662ad60f58564a8bd
xervii <ahtaullahakmali@gmail.com>
2026-05-18 20:38:48 +0700

    docs(arena): Add clarity in header file

--- arena.h
+++ arena.h
@@ -6,7 +6,7 @@
 typedef struct Arena {
     char *base;
     size_t totalSize;
-    size_t offset;
+    size_t offset;  //could be int or size_t
 } Arena;
 
 Arena arenaInit(size_t arenaSize);
@@ -15,7 +15,7 @@ char* arenaAlloc(Arena *arena, size_t allocSize);
 char* arenaGet(Arena *arena, size_t offset);    //convert offset->ptr
 void arenaReset(Arena *arena);
 
-void arenaPrint(Arena *arena);
+void arenaPrint(Arena *arena);  //per 2 bytes visuallization
 void arenaFree(Arena *arena);
 
 #endif
