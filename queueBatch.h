970602194ea4911ade08a434d6f7fca816df9940
xervii <ahtaullahakmali@gmail.com>
2026-05-21 17:11:48 +0700

    feat(queue): Finish queue mandatory functions

--- queueBatch.h
+++ queueBatch.h
@@ -19,8 +19,10 @@ void queue_init(Queue *queue);
 int queue_create(Arena *arena);
 int create_job(Arena *arena, int jobId, const char *documentName);
 void search_job(Arena *arena, Queue *queue, int targetId);
+
 void enqueue(Arena *arena, Queue *queue, int jobOffset);
-void dequeue(Arena *arena, Queue *queue);
+int dequeue(Arena *arena, Queue *queue);
+
 void display_queue(Arena *arena, Queue *queue);
 void process_batch(Arena *arena, Queue *queue, int batchSize);
 
