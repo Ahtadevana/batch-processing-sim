b59ed196c974562af4ed296e9beea3e2358a24b0
xervii <ahtaullahakmali@gmail.com>
2026-05-22 18:19:14 +0700

    feat(queue): Add queue display and batch process system

--- queueBatch.c
+++ queueBatch.c
@@ -9,8 +9,10 @@ void queue_init(Queue *queue);
 int queue_create(Arena *arena);
 int create_job(Arena *arena, int jobId, const char *documentName);
 void search_job(Arena *arena, Queue *queue, int targetId);
+
 void enqueue(Arena *arena, Queue *queue, int jobOffset);
-void dequeue(Arena *arena, Queue *queue);
+int dequeue(Arena *arena, Queue *queue);
+
 void display_queue(Arena *arena, Queue *queue);
 void process_batch(Arena *arena, Queue *queue, int batchSize);
 */
@@ -45,4 +47,76 @@ int create_job(Arena *arena, int jobId, const char *documentName) {
     int newJobOffset = (char*)newJob - arena->base;   //job offset from base
 
     return newJobOffset;
+}
+
+void enqueue(Arena *arena, Queue *queue, int jobOffset) {
+    if(queue->rear == NULL_OFFSET){
+        queue->front = jobOffset;
+        queue->rear = jobOffset;
+        return;
+    }
+
+    PrintJob *newJob = (PrintJob*)arena_get(arena, queue->rear);
+
+    newJob->next = jobOffset;
+    queue->rear = jobOffset;
+}
+
+int dequeue(Arena *arena, Queue *queue) {
+    if(queue->front == NULL_OFFSET){
+        printf("Queue is empty.\n");
+        return NULL_OFFSET;
+    }
+
+    int removedOffset = queue->front;
+
+    PrintJob *frontJob = (PrintJob*)arena_get(arena, queue->front);
+    queue->front = frontJob->next;
+    
+    if(queue->front == NULL_OFFSET){
+        queue->rear = NULL_OFFSET;
+    }
+
+    return removedOffset;
+}
+
+void display_queue(Arena *arena, Queue *queue) {
+    if(arena->base == NULL){
+        printf("displayQueue: Arena is uninitiallized.\n");
+        return;
+    }
+
+    if(queue->front == NULL_OFFSET){
+        printf("displayQueue: Queue is empty.\n");
+        return;
+    }
+
+    int current = queue->front;
+
+    printf("===Queue Jobs===\n");
+    while(current != NULL_OFFSET){
+        PrintJob *job = (PrintJob*)arena_get(arena, current);
+
+        printf("Job ID: %d | Name: %s\n", job->id, job->document);
+        current = job->next;
+    }
+
+    printf("================\n");
+}
+
+void process_batch(Arena *arena, Queue *queue, int batchSize) {
+    printf("===Process Batch===\n");
+
+    for(int i=0; i < batchSize; i++){
+        int jobOffset = dequeue(arena, queue);
+        if(jobOffset == NULL_OFFSET){
+            break;
+        }
+
+        PrintJob *job = (PrintJob*)arena_get(arena, jobOffset);
+
+        printf("Processing job ID: %d | Name: %s . . .\n", job->id, job->document);
+    }
+
+    printf("===================\n");
 }
