3927123b77a2516723a529c7f582348b0791e432
xervii <ahtaullahakmali@gmail.com>
2026-05-23 20:19:35 +0700

    test(main): Call interactive

--- main.c
+++ main.c
@@ -21,22 +21,6 @@ void process_batch(Arena *arena, Queue *queue, int batchSize);
 */
 
 int main()  {
-    Arena mainArena = arena_init(128);
-
-    int queueOffset = queue_create(&mainArena);
-    Queue *queue = (Queue*)arena_get(&mainArena, queueOffset);  //get my offset to pointer
-    arena_print(&mainArena);
-
-    int job1 = create_job(&mainArena, 101, "Cat.png");
-    int job2 = create_job(&mainArena, 102, "Report.pdf");
-    int job3 = create_job(&mainArena, 103, "Scenery.png");
-    arena_print(&mainArena);
-
-    enqueue(&mainArena, queue, job1);
-    enqueue(&mainArena, queue, job2);
-    enqueue(&mainArena, queue, job3);
-    display_queue(&mainArena, queue);
-
-    process_batch(&mainArena, queue, 3);
+    interactive();
     return 0;
 }
